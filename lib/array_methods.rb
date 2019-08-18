5f02dfc9de15207f52bc9daceeb44b8be934311d
Christina Gonzalez <cmgamway@gmail.com>
2019-08-18 05:09:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.include?("wow")
-  => 
+  =>
   array.include?("Tardis")
-  => 
+  =>
 
 end
 
